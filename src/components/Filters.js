53c901c0b35fe49e57747c4ff8b2bed1172fb595
Stephen Lander <stephenclander@gmail.com>
2020-06-15 14:08:42 -0600

    connect local to github

---
+++ src/components/Filters.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function Filters() {
+    return (
+        <div class="filters">
+            <p>Filters</p>
+        </div>
+    );
+}
+
+export default Filters;
