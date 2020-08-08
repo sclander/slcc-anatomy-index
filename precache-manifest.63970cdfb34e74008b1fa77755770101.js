fe123bda9560ea65ed1101ca7539a2275f57aa96
Stephen Lander <stephenclander@gmail.com>
2020-08-08 02:16:33 -0600

    Updates

---
+++ precache-manifest.63970cdfb34e74008b1fa77755770101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac73d1adb0d5a01520602baf43cd66f4",
+    "url": "/slcc-anatomy-index/index.html"
+  },
+  {
+    "revision": "ac77a171360900d8e6f6",
+    "url": "/slcc-anatomy-index/static/css/main.d6f2110a.chunk.css"
+  },
+  {
+    "revision": "160a8d2253155c602577",
+    "url": "/slcc-anatomy-index/static/js/2.a33080cb.chunk.js"
+  },
+  {
+    "revision": "4b6ff87288029a7b288110f445284443",
+    "url": "/slcc-anatomy-index/static/js/2.a33080cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac77a171360900d8e6f6",
+    "url": "/slcc-anatomy-index/static/js/main.8f2f8b53.chunk.js"
+  },
+  {
+    "revision": "efedc3d1ad0dcadcce24",
+    "url": "/slcc-anatomy-index/static/js/runtime-main.b21f07e1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/slcc-anatomy-index/precache-manifest.63970cdfb34e74008b1fa77755770101.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/slcc-anatomy-index/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
